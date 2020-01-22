a1acdd567c1b7efd4a8f3561f063a0dfd4a0fe2b
b1gbroth3r <b1gbroth3r@protonmail.com>
2020-01-22 12:49:43 -0600

    Added verified vulns and structured output

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # shoMe
 A Python script that queries a list of IPs and returns useful or interesting information (for externals/webapps)
+Example: ```python3 shoMe.py <file_of_IPs> <output_file>```
--- shoMe.py
+++ shoMe.py
@@ -3,12 +3,34 @@ import shodan
 import textwrap
 from sys import *
 
-
-API_KEY = "API KEY GOES HERE"
+API_KEY = "ENTER YOUR API KEY HERE"
 HIGH_PROFILE_PORTS = [21, 22, 23, 389, 445, 1443, 3306, 3389, 5432, 5432, 27017]
 INTERESTING_TARGETS = []
 
+def output_ip_port(outfile, lyst):
+    outfile.write("#" * 70 + "\n")
+    outfile.write("IP and Port information listed here: \n")
+    for info in lyst:
+        outfile.write(info + "\n")
+    outfile.write("#" * 70 + "\n")
+
+def output_webserver(outfile, lyst):
+    outfile.write("Web server information listed here: \n")
+    for webserv in lyst:
+        #print(webserv)
+        outfile.write(webserv + "\n")
+    outfile.write("#" * 70 + "\n")
+
+def output_vulns(outfile, lyst):
+    outfile.write("Verified vulnerabilities listed here: \n")
+    for vuln in lyst:
+        outfile.write(vuln + "\n")
+    outfile.write("#" * 70 + "\n")
+
 def shoMe(infile, outfile):
+    ip_port_list = []
+    webserver_list = []
+    vulns_list = []
     with open(infile, "r") as ips:
         with open(outfile, "w") as out:
             for ip in ips:
@@ -22,14 +44,17 @@ def shoMe(infile, outfile):
                     if ("Server: Microsoft-IIS" in x["data"]):
                         tmp = str(x["data"]).split("Server: Microsoft-IIS")[1].split("\n")[0].replace("/", "").replace("\r", "")
                         iis_result = "IP: {}; IIS-Version: {} ".format(x['ip_str'], tmp)
-                        out.write(iis_result + "\n")
+                        webserver_list.append(iis_result)
                     if ("X-Powered-By: PHP" in x['data']):
                         tmp = str(x["data"]).split("X-Powered-By: PHP")[1].split("\n")[0].replace("/", "").replace("\r", "")
                         php_result = "IP: {}; PHP Version: {} ".format(x['ip_str'], tmp)
-                        out.write(php_result + "\n")
+                        webserver_list.append(php_result)
                     if ("vulns" in x.keys()):
-                        print("IP: {} HAS VULNS".format(x['ip_str']))
-                        # TODO Figure out a way to traverse the nested dictionaries and pull out only vulns that are listed as verified
+                        tmp = x['vulns']
+                        for k,v in tmp.items():
+                            if (v['verified'] == True):
+                                vulns = x['ip_str'] + ' ' + k + ' ' + str(v['verified'])
+                                vulns_list.append(vulns)
                 if (info != None):
                     ret += info['ip_str']
                     ret += "; Ports: "
@@ -38,9 +63,12 @@ def shoMe(infile, outfile):
                         if port in HIGH_PROFILE_PORTS:
                             INTERESTING_TARGETS.append((info['ip_str'], port))
                         ret += str(port) +  ", "
-                    out.write(ret[:-2] + "\n")
+                    ip_port_info = ret[:-2]
+                    ip_port_list.append(ip_port_info)
                     info = None
-
+            output_ip_port(out, ip_port_list)
+            output_webserver(out, webserver_list)
+            output_vulns(out, vulns_list)
             out.write("These IP addresses have interesting ports open: \n")
             for hv in INTERESTING_TARGETS:
                 out.write("IP: {}; Port: {}".format(hv[0], hv[1]) + "\n")
